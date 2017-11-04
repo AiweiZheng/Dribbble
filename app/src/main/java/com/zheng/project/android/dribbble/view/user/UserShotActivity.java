6ae361dd693cda6d21ce4dcaf6ccb9cd4af59a9e
Aiwei Zheng <aiweizheng@gmail.com>
2017-11-04 17:17:45 -0400

    implemented view user's shots feature.

--- app/src/main/java/com/zheng/project/android/dribbble/view/user/UserShotActivity.java
+++ app/src/main/java/com/zheng/project/android/dribbble/view/user/UserShotActivity.java
@@ -3,27 +3,34 @@ package com.zheng.project.android.dribbble.view.user;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
+import com.google.gson.reflect.TypeToken;
 import com.zheng.project.android.dribbble.R;
+import com.zheng.project.android.dribbble.models.User;
+import com.zheng.project.android.dribbble.utils.ModelUtils;
 import com.zheng.project.android.dribbble.view.base.SingleFragmentActivity;
 import com.zheng.project.android.dribbble.view.shot_list.ShotListFragment;
 
 public class UserShotActivity extends SingleFragmentActivity {
+
     @Override
     public String getActivityTitle() {
-        return getIntent().getStringExtra(UserInfoFragment.KEY_AUTHOR_NAME);
+        return getUser().name;
     }
 
     @Override
     protected String getSubtitle() {
-        return getString(R.string.shots) + " (" +
-                getIntent().getIntExtra(UserInfoFragment.KEY_AUTHOR_SHOT_COUNT, 0) + ")";
+        return getString(R.string.shots) + " (" + getUser().shots_count + ")";
     }
 
     @NonNull
     @Override
     protected Fragment newFragment() {
-        Fragment fragment = ShotListFragment.newUserShotListInstance(
-                getIntent().getStringExtra(UserInfoFragment.KEY_USER));
+        Fragment fragment = ShotListFragment.newUserShotListInstance(getUser());
         return fragment;
+    }
+
+    private User getUser() {
+        return ModelUtils.toObject(getIntent().getStringExtra(UserInfoFragment.KEY_USER),
+                new TypeToken<User>(){});
     }
 }
